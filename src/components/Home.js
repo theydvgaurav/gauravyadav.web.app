3c2b967b94a2b1e6cc75355ea2a84c3f6c985238
Gaurav Yadav <gauravyadav2609@gmail.com>
2021-03-29 00:15:12 +0530

    Projects

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Typed from 'react-typed'
-import Particles from 'react-particles-js'
 function Home() {
     return (
+        <div className='b_g'>
         <div className = "header-wraper">
         <div className = "main-info">
             <Typed className = "typed-text" 
@@ -11,20 +11,11 @@ function Home() {
             backSpeed = {70}
             loop
             />
-            <Particles
-            params={{
-
-                polygon: {
-                    enable: true,
-                    type: 'inside',
-                    number : 30,
-                    value_area : 900,
-                    move: {
-                        radius: 10
-                    },
-                    url: 'path/to/svg.svg'
-                }}}/>
+            </div>                              
+         <div className = "main-info">
+            
         </div>      
+        </div>
         </div>
     )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,47 @@
 import React from 'react'
-
+import ppic1 from './Ax.jpeg'
+import ppic2 from './Textpad.png'
 function Projects() {
     return (
-        <div>
-            This is Project Page
+        <div className="container">
+            <br/>
+           <div className='card c_d'>
+                <div class="card-body">
+                <div className="container-fluid">
+                <div className="row align-items-start">
+                <div className="col c_d">
+                    <a href="https://github.com/theydvgaurav/AppointX-Source-Code"> <img src = {ppic1} className="img-thumbnail" alt = {"Logo"}></img> </a>
+                </div>
+                <div className="col">
+                <div className="main-info">
+                <p>
+                An interactive web app for online doctor appointments. A new way to replace the traditional appointment system by real-time appointment booking. This web-based application overcomes the issue of managing and booking appointments according to patient’s needs.
+                </p>    
+                </div>
+                </div>
+                </div>
+                </div>
+                </div>
+           </div>
+           <br/>
+           <div className='card c_d'>
+                <div class="card-body">
+                <div className="container-fluid">
+                <div className="row align-items-start">
+                <div className="col c_d">
+                    <a href="https://github.com/theydvgaurav/Qt_Textpad"><img src = {ppic2} className="img-thumbnail" alt = {"Logo"}></img></a>
+                </div>
+                <div className="col">
+                <div className="main-info">
+                <p>
+                Developed a Desktop application similar to Notepad using Qt Creator. The GUI was developed using the Qt framework, with functionalities using C++. The GUI and functionality of the Project utilize OOPs Concepts.
+                </p>
+                </div>
+                </div>
+                </div>
+                </div>
+                </div>
+           </div>
         </div>
     )
 }
