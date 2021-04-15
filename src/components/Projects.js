8e5b957bb92e938ea62d9126c5fdde3f98b1a424
Gaurav Yadav <gauravyadav2609@gmail.com>
2021-04-15 10:04:06 +0530

    C++ Project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ppic1 from './Ax.jpeg'
 import ppic2 from './Textpad.png'
+import ppic3 from './Banking-System.png'
 function Projects() {
     return (
         <div className="container">
@@ -36,6 +37,27 @@ function Projects() {
                 <p>
                 Developed a Desktop application similar to Notepad using Qt Creator. The GUI was developed using the Qt framework, with functionalities using C++. The GUI and functionality of the Project utilize OOPs Concepts.
                 </p>
+                </div>
+                </div>
+                </div>
+                </div>
+                </div>
+           </div>
+           <br/>
+           <div className='card c_d'>
+                <div class="card-body">
+                <div className="container-fluid">
+                <div className="row align-items-start">
+                <div className="col c_d">
+                    <a href="https://github.com/theydvgaurav/Banking-System"> <img src = {ppic3} className="img-thumbnail" alt = {"Logo"}></img> </a>
+                </div>
+                <div className="col">
+                <div className="main-info">
+                <p>
+                This is Command Line Banking-System Project written in C++. There are two major roles here Admin and the User. 
+                In this program, the concepts of C++ classes and objects are also used.
+                It has several functionalities such as Opening an account, Deleting an Account, Amount Deposition, Amount Withdrawl, and Amount Trasfer.
+                </p>    
                 </div>
                 </div>
                 </div>
